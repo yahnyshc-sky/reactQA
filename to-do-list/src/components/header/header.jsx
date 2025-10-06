ec1efa974bd33349c4d3f1f0e5663e26cf857ff5
Daveraj <daveraj.nothay@sky.uk>
2025-10-06 10:24:04 +0100

    CREATE: added header, QA logo, and started tests

---
+++ to-do-list/src/components/header/header.jsx
@@ -0,0 +1,52 @@
+import './header.css';
+import React from 'react';
+import { NavLink} from 'react-router';
+import logo from '../../assets/qalogo.svg';
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md" style={{ backgroundColor: "#14EAB8" }}>
+                <a
+                    className="navbar-brand d-flex align-items-center"
+                    href="https://www.qa.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={logo} alt="QA Ltd" width="100" className="qa-logo me-2" />
+                </a>
+
+                <a className="navbar-brand">
+                    <h1>Todo App</h1>
+                </a>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto">
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive ? "nav-link active" : "nav-link"
+                                }
+                            >
+                                All Todos
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/add"
+                                className={({ isActive }) =>
+                                    isActive ? "nav-link active" : "nav-link"
+                                }
+                                data-testid="addTodoLink"
+                            >
+                                Add Todo
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
