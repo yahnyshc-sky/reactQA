9d5388075fc48370efd13ee5e333054da69d7c8f
Daveraj <daveraj.nothay@sky.uk>
2025-10-07 10:23:02 +0100

    UPDATE: changed how the todo routes and services are used

--- to-do-backend/services/todo_service.py
+++ to-do-backend/services/todo_service.py
@@ -1,3 +1,6 @@
+from db.data_access import DataAccess
+from flask import jsonify
+
 def update_todo(data):
     id = data['id']
     description = data['description']
@@ -5,3 +8,25 @@ def update_todo(data):
 
 def get_todo(todo_id):
     return None
+
+def add_todo(data):
+    description = data.get('description')
+    completed = data.get('completed', False)
+
+    if not description:
+        return jsonify({'error': 'Description is required'}), 400
+
+    try:
+        dao = DataAccess()
+        dao.execute("INSERT INTO todo (description, completed) VALUES (%s, %s);",(description, completed))
+        return jsonify({'id': dao.lastrowid, 'description': description, 'completed': completed}), 201
+    except Exception as e:
+        raise e 
+
+def get_all_todos():
+    try:
+        dao = DataAccess()
+        todos = dao.query("SELECT id, description, created_at, completed FROM todo ORDER BY created_at DESC;")
+        return todos
+    except Exception as e:
+        raise e
